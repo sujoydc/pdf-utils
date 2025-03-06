08278ab9393e2bd55cec0205147d2d565d059268
Sujoy Datta Choudhury <sujoydc@gmail.com>
2025-03-06 00:20:51 -0800

    UPdated API

--- src/main/java/org/sujix/pdfx/pdfutils/controller/PDFController.java
+++ src/main/java/org/sujix/pdfx/pdfutils/controller/PDFController.java
@@ -2,15 +2,17 @@ package org.sujix.pdfx.pdfutils.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1")
 public class PDFController {
     
-    @GetMapping
-    public void check(){
-        
+    @GetMapping(value = "/status")
+    @ResponseBody       
+    public String check(){
+        return "PDFx util is working!!!";
     }
 
 }
